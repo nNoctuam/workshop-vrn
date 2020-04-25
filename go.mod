61412b39e8c084a24476bbd1cc5c6aaa3920c8b5
nnoctuam <nnoctuam@gmail.com>
2020-04-25 14:41:38 +0300

    Tests

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module workshop
 go 1.14
 
 require (
-	github.com/go-chi/chi v4.1.1+incompatible // indirect
-	github.com/ilyakaznacheev/cleanenv v1.2.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/chi v4.1.1+incompatible
+	github.com/ilyakaznacheev/cleanenv v1.2.2
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.2.2
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
---
+++ internal/handler/handler_test.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"workshop/internal/api"
+	"workshop/internal/api/mocks"
+)
+
+// https://github.com/vektra/mockery
+// $GOPATH/bin/mockery -name=Client -dir=internal/api -output=internal/api/mocks
+
+func TestHandler_Hello(t *testing.T) {
+	tests := []struct {
+		name     string
+		joke     *api.JokeResponse
+		err      error
+		codeWant int
+		bodyWant string
+	}{
+		{
+			name:     "simple test",
+			joke:     &api.JokeResponse{"test joke"},
+			err:      nil,
+			codeWant: 200,
+			bodyWant: "test joke",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiMock := &mocks.Client{}
+			apiMock.On("GetJoke").Return(tt.joke, tt.err)
+			h := NewHandler(apiMock)
+			req, _ := http.NewRequest("GET", "/hello", nil)
+			rr := httptest.NewRecorder()
+
+			h.Hello(rr, req)
+
+			gotRaw, _ := ioutil.ReadAll(rr.Body)
+			got := string(gotRaw)
+			if got != tt.bodyWant {
+				t.Errorf("wrong response body %s want %s", got, tt.bodyWant)
+			}
+
+			if status := rr.Result().StatusCode; status != tt.codeWant {
+				t.Errorf("wrong response status %d want %d", status, tt.codeWant)
+			}
+
+		})
+	}
+}
